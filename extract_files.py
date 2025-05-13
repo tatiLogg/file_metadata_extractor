052b2b7ef6b79d9091a51f2a684a72d07f1c9256
Selina Loggins <157069359+tatiLogg@users.noreply.github.com>
2025-05-13 12:44:45 -0500

    Refactor to function with recursive directory scanning

--- extract_files.py
+++ extract_files.py
@@ -1,11 +1,24 @@
 import os
 
-files_info = []
+def extract_file_info(path='.'):
+    """
+    Recursively collects file information starting from the given path.
+    Returns a list of dictionaries with file path and size.
+    """
+    files_info = []
 
-for file_name in os.listdir():
-    if os.path.isfile(file_name):
-        file_size = os.path.getsize(file_name)
-        files_info.append({'path': file_name, 'size': file_size})
+    for root, _, files in os.walk(path):
+        for file in files:
+            full_path = os.path.join(root, file)
+            size = os.path.getsize(full_path)
+            files_info.append({'path': full_path, 'size': size})
 
-print(files_info)
+    return files_info
+
+# Test the function
+if __name__ == "__main__":
+    results = extract_file_info()
+    for item in results:
+        print(item)
+
 
