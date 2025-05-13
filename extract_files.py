76dcd3b2711ff3b47b63f26957f886c1913744c2
Selina Loggins <157069359+tatiLogg@users.noreply.github.com>
2025-05-13 12:48:22 -0500

    Add nested dictionary structure for recursive file data

--- extract_files.py
+++ extract_files.py
@@ -1,24 +1,29 @@
 import os
 
-def extract_file_info(path='.'):
+def extract_file_info_nested(path='.'):
     """
-    Recursively collects file information starting from the given path.
-    Returns a list of dictionaries with file path and size.
+    Recursively collects file information and organizes it in a nested dictionary.
+    Keys are folder paths, and values are dictionaries of {filename: {path, size}}.
     """
-    files_info = []
+    nested_info = {}
 
     for root, _, files in os.walk(path):
+        relative_root = os.path.relpath(root, path)
+        nested_info[relative_root] = {}
+
         for file in files:
             full_path = os.path.join(root, file)
             size = os.path.getsize(full_path)
-            files_info.append({'path': full_path, 'size': size})
+            nested_info[relative_root][file] = {
+                'path': full_path,
+                'size': size
+            }
 
-    return files_info
+    return nested_info
 
 # Test the function
 if __name__ == "__main__":
-    results = extract_file_info()
-    for item in results:
-        print(item)
-
+    nested = extract_file_info_nested()
+    from pprint import pprint
+    pprint(nested)
 
